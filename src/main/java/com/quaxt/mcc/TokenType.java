0ca0d05236e7379b85833a3f8d2a8e2f3e735d32
Michael Reilly <mikeyreilly@gmail.com>
2025-01-01 15:39:19 +0000

    Chapter 3 lex

--- src/main/java/com/quaxt/mcc/TokenType.java
+++ src/main/java/com/quaxt/mcc/TokenType.java
@@ -7,7 +7,9 @@ public enum TokenType {
     OPEN_PAREN("\\("), CLOSE_PAREN("\\)"),
     OPEN_BRACE("\\{"), CLOSE_BRACE("\\}"), NUMERIC("[0-9]+\\b"), SEMICOLON(";"), SINGLE_LINE_COMMENT("//.*"),
     MULTILINE_COMMENT(Pattern.compile("/\\*.*\\*/", Pattern.DOTALL)),
-    INT("int"), RETURN("return"), VOID("void"), DECREMENT("--"), NEGATE("-"), COMPLIMENT("~");
+    INT("int"), RETURN("return"), VOID("void"), DECREMENT("--"),INCREMENT("\\+\\+"), NEGATE("-"), COMPLIMENT("~"),
+    PLUS("\\+"),
+    MULTIPLY("\\*"),DIVIDE("/"),REMAINDER("%");
 
     Pattern regex;
 
--- src/main/java/com/quaxt/mcc/asm/Instruction.java
+++ src/main/java/com/quaxt/mcc/asm/Instruction.java
@@ -1,7 +1,5 @@
 package com.quaxt.mcc.asm;
 
-import java.io.PrintWriter;
-
 public sealed interface Instruction permits AllocateStack, Mov, Nullary, Unary {
 
 
